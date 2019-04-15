13940a64e8ccf168f1a84bd1e5ef1c66ee726958
dth27 <dth27@hi.is>
2019-04-15 16:18:11 +0000

    Firsta commit

---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="full-width center-content">
+    <hello-component name="World" />
+    <img src="../static/img/ThelmaHeadshot.jpeg"><img/>
+  </div>
+</template>
+<script>
+import HelloComponent from './components/HelloComponent.vue'
+export default {
+  components: {
+    HelloComponent
+  }
+}
+</script>
